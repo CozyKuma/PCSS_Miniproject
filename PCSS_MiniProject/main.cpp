755df2c7e1c247dda0b1556fee951510904464a4
Simon Nielsen <sgfn17@student.aau.dk>
2018-10-26 10:26:40 +0200

    Import of main stuff shit stuff

--- PCSS_MiniProject/main.cpp
+++ PCSS_MiniProject/main.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include <string>
+#include <time.h>
+
+
+#include <CombatSystem.h>
+#include <Character.h>
+
+// Races for the Character
+#include <Human.h>
+#include <Wombat.h>
+#include <Elf.h>
+
+// Classes the Character can pick
+#include <Wizard.h>
+#include <Fighter.h>
+#include <Ranger.h>
+
 
 using namespace std;
 
