a3211245aac3c322d508e0d7eef2e4b243fc05f6
esandk17 <esandk17@student.aau.dk>
2018-10-31 11:56:00 +0100

    Update PCSS_MiniProject/main.cpp

--- PCSS_MiniProject/main.cpp
+++ PCSS_MiniProject/main.cpp
@@ -8,34 +8,71 @@ int main(){
 
 }
 
+
+
 class CombatSystem{
 
 
     void startCombat(Character c1, Character c2){
 
         bool gameRuns = true;
+        bool initiativeRolled = false;
 
         int player1 = 0;
         int player2 = 0;
 
         Character turnOrder[2];
-
-
+        Character player1;
+        Character player2;
+        bool swapTurn = false,
         while(gameRuns){
 
-            rollInitiative(); //skal kun gøres en gang
-
-            Character activePlayer;
-            for(int i = 0; i < 2; i++){
-
-                activePlayer = turnOrder[i];
-
-
+            if(initiativeRolled == false){
+                rollInitiative();
+                player1 = turnOrder[0];
+                player2 = turnOrder[1];
             }
 
+            if(swapTurn == false){
+                activePlayer = player1;
+                inactivePlayer = player2;
+                swapTurn = true;
+            }
+            else{
+                activePlayer = player2;
+                inactivePlayer = player1;
+            }
+
+            cout << "You currently have: " << activePlayer.health << " HP" << endl;
+            cout << "Choose your ability by entering 1, 2 or 3" << endl;
+            cin >> abilityNumber;
+            activePlayer.chooseAbility(abilityNumber);
+
+            checkHealth(player1, player2)
 
 
         }
+
+    void checkHealth(Character activePlayer, inactivePlayer){
+        if (activePlayer.health <= 0){
+            activePlayer.alive = false;
+            cout << inactivePlayer.getName() <<" has triumphed!" << endl;
+            cout << "You have lost" << endl;
+
+        }
+        else if(inactivePlayer.health <= 0){
+            inactivePlayer.alive = false;
+            cout << "You have won!" << endl;
+        }
+        else{return}
+
+    }
+
+    void restart(){
+        string exitString;
+        cout << "Do you want play again? (y/n)" << endl;
+        cin >> exitString;
+    }
 
     void combatRollInitiative(){
 
