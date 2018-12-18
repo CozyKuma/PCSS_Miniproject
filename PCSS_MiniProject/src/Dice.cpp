9055283d6778814c96c5206428581c31b098a215
Simon Nielsen <sgfn17@student.aau.dk>
2018-12-18 11:48:23 +0100

    Update PCSS_MiniProject/src/Dice.cpp
    Dice update

--- PCSS_MiniProject/src/Dice.cpp
+++ PCSS_MiniProject/src/Dice.cpp
@@ -1,14 +1,15 @@
 #include "Dice.h"
-
+#include <cstdlib>
 
 
 int Dice::rollDice(int amount, int sides)
 {
+    srand (time(NULL));
     int result = 0;
     for (int i = 0; i < amount; i++)
     {
        // result = result + rand()% sides +1;
-        result = amount + sides;
+        result = result + (rand() % sides) + 1;
     }
     return result;
 
